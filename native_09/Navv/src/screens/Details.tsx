e55060d8cd80717ee60cd610bad70d5cac651807
shohanurshihab <skshihab531@gmail.com>
2024-04-21 15:41:08 +0600

    attempt project 09

---
+++ native_09/Navv/src/screens/Details.tsx
@@ -0,0 +1,35 @@
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NativeStackNavigationProp, NativeStackScreenProps } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../App'
+import { useNavigation } from '@react-navigation/native'
+
+type DetailsProps = NativeStackScreenProps<RootStackParamList, 'Details'>
+
+const Details = ({route}: DetailsProps) => {
+  const {productId} = route.params
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>()
+  return (
+    <View style={styles.container}>
+      <Text>Details: {productId} </Text>
+      <Button 
+      title='Go to Home' 
+      // onPress={() => navigation.navigate('Home')} 
+      onPress={() => navigation.popToTop()} 
+      />
+    </View>
+  )
+}
+
+export default Details
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    smallText: {
+        color: "#000000"
+    }
+})
